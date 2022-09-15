32323fbc460e34a476273cba9199a103acbd5e69
Jiron <59139789+Jiron@users.noreply.github.com>
2022-09-15 16:34:47 +0200

    feat: 404 page

---
+++ frontend/src/404Page.js
@@ -0,0 +1,12 @@
+import './404Page.css';
+
+function PageNotFound() {
+  return (
+    <div class="c-404page">
+        <h1>404 - Not Found</h1>
+        <a href="/">Back To Home</a>
+    </div>
+  );
+}
+
+export default PageNotFound;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import PageNotFound from './404Page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>}></Route>
+        <Route path="*" element={<PageNotFound/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
