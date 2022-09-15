8a747c101a7cce6ddae27fe0f896c9b5f92434e5
Jiron <59139789+Jiron@users.noreply.github.com>
2022-09-15 12:38:49 +0200

    feat: correct user auth

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const auth = require('json-server-auth');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const jwt_decode = require('jwt-decode');
 
 server.use(middlewares);
 
@@ -10,9 +11,19 @@ server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
   if (req.method === 'POST') {
     req.body.createdAt = Date.now();
+    if(req.url == "/tasks" && req.headers.authorization && req.headers.authorization.split(' ')[1]) {
+      let token = req.headers.authorization.split(' ')[1];
+      let decoded = jwt_decode(token);
+      req.body.userId = parseInt(decoded.sub);
+    }
   }
   if (req.method === 'PUT') {
     req.body.editedAt = Date.now();
+    if(req.url == "/tasks" && req.headers.authorization && req.headers.authorization.split(' ')[1]) {
+      let token = req.headers.authorization.split(' ')[1];
+      let decoded = jwt_decode(token);
+      req.body.userId = decoded.sub;
+    }
   }
   next();
 });
@@ -21,7 +32,7 @@ server.db = router.db;
 
 const rules = auth.rewriter({
   users: 600,
-  tasks: 440,
+  tasks: 600,
 });
 
 server.use(rules);
